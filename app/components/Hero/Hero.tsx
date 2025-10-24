9411ebc7061d7b8cba4fb0ed15de23cc3a003b1e
shayanmahmood <shayanmahmood234@gmail.com>
2025-10-24 05:54:21 -0700

    Figma conversation home page complete

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -9,7 +9,7 @@ export default function Hero() {
     <section
       className="relative max-w-[1600px] mx-auto grid grid-cols-1 lg:grid-cols-[5fr_6fr] 
                  items-center px-4 sm:px-8 md:px-12 lg:px-20 gap-10 lg:gap-12 
-                 overflow-hidden min-h-[80vh] lg:min-h-[calc(100vh-80px)]"
+                 overflow-hidden min-h-[80vh] lg:min-h-[calc(100vh-80px)] lg:pr-0 lg:mr-0"
     >
       {/* Left Side */}
       <div className="flex justify-center lg:justify-start items-center lg:-translate-y-0">
@@ -17,7 +17,7 @@ export default function Hero() {
       </div>
 
       {/* Right Side */}
-      <div className="relative w-full h-[400px] sm:h-[420px] md:h-[600px] lg:h-[720px] overflow-hidden">
+      <div className="relative w-full h-[400px] sm:h-[420px] md:h-[600px] lg:h-[720px] overflow-hidden ">
         <HeroCarousel />
         <SocialIcons />
       </div>
