986001c68c5c4c182ac8f1c8bfcfa51b1192fb68
shayanmahmood <shayanmahmood234@gmail.com>
2025-10-29 08:56:00 -0700

    home page fixes

--- app/components/Home/Hero/Hero.tsx
+++ app/components/Home/Hero/Hero.tsx
@@ -8,9 +8,9 @@ export default function Hero() {
   return (
     <section
       className="relative max-w-[1600px] mx-auto grid grid-cols-1 lg:grid-cols-[5fr_6fr] 
-             items-center px-4 sm:px-8 md:px-12 lg:px-20 gap-10 lg:gap-12 
+             items-center px-4 sm:px-8 md:px-12 lg:px-0 gap-10 lg:gap-12  lg:
              overflow-hidden min-h-[80vh] lg:min-h-[calc(100vh-80px)] 
-             lg:pr-0 lg:mr-0 2xl:pr-auto 2xl:mr-auto"
+             lg:pr-0 lg:mr-0  2xl:pr-auto 2xl:mr-auto  2xl:max-w-[1250px]"
     >
       {/* Left Side */}
       <div className="flex justify-center lg:justify-start items-center lg:-translate-y-0">
